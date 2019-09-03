3540e3eec1b1dfc339babc56f9cc62d537deefc2
NageshJoy007 <Nagesh.joy007@gmail.com>
2019-09-03 19:43:53 +0530

    updated for listners

--- src/main/java/com/utils/RetryAnalyzer.java
+++ src/main/java/com/utils/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer{
 
 	int counter =0;
-	int retryLimit = 0;
+	int retryLimit = 1;
 	@Override
 	public boolean retry(ITestResult arg0) {
 		if(counter<retryLimit){
