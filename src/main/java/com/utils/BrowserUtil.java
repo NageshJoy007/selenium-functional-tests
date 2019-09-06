bf0c4f9da016f7fd8149600fec97e86b65f5a2ab
NageshJoy007 <Nagesh.joy007@gmail.com>
2019-09-06 22:38:40 +0530

    Adding headless conf for gc n ff

--- src/main/java/com/utils/BrowserUtil.java
+++ src/main/java/com/utils/BrowserUtil.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -17,24 +18,34 @@ public class BrowserUtil {
 	public WebDriver driver;
 
 	@Parameters({ "browserName" })
-
 	@BeforeMethod(alwaysRun = true)
 	public void openBroser(String browserName) {
+		
+		// you can also pass browser name from command line $ mvn test -DbrowserName=Firefox/Chrome
+			String cliBrowserName = System.getProperty("browserName");
+			if(cliBrowserName!=null) {
+			   browserName =cliBrowserName;
+			 }
 
 		if (browserName.toLowerCase().startsWith("c")) {
 
 			ChromeOptions options = new ChromeOptions();
-			// uncomment below piece of code to run your tests in headless mode
-			/* options.addArguments("headless"); */
+		// 	below line of code to run your tests in headless mode
+			 options.addArguments("headless");
+			 
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver(options);
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		} else if (browserName.toLowerCase().startsWith("f")) {
-
+			
+			FirefoxOptions options = new FirefoxOptions();
+		// 	below line of code to run your tests in headless mode
+			 options.addArguments("-headless");
+			 
 			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			driver = new FirefoxDriver(options);
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		}
